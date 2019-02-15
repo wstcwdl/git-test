38b54cdd19abdd69d8c65a3ecf4f6bacd96b84ee
linwenjie <linwjwy@163.com>
2019-02-15 20:16:28 +0800

    第二次提交

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
         System.out.println("hello git project");
+        System.out.println("hello git project");
     }
 }
