66189f596e1fec6f6455ef1a55f8fa5eb391d6d7
wstcwdl <47655163+wstcwdl@users.noreply.github.com>
2019-02-16 08:33:16 +0800

    Update FirstClass.java

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -10,5 +10,6 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println(333);
         System.out.println(44222);
+        System.out.println("hello git project");
     }
 }
